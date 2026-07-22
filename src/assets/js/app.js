581fafdcba0d3a0b4a3f240cd8eb3520a3f64dae
moheetdigital <moheet.digital@gmail.com>
2026-07-23 00:01:55 +0300

    fix bugs

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -87,9 +87,8 @@ class App extends AppHelpers {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
-
-        
-          observer.unobserve(entry.target);
+        } else {
+          entry.target.classList.remove("show");
         }
       });
     },
